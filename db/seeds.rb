03d4a77dae5aa26d318280a720bac087cabd5c2c
alexsaintlam <alexlam@DESKTOP-ILBQ44R.localdomain>
2021-09-09 16:52:14 -0400

    debugging heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,37 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Pinstoboard.destroy_all
+Pin.destroy_all
+Board.destroy_all
+Follow.destroy_all
 User.destroy_all
+
     user5 = User.create!(username: "demoUser", email: "demo@demo.com", age: 1, password: "demo123")
     user1 = User.create!(username: "alex", email: "alex@alex.com", age: 31, password: "alex123")
     user2 = User.create!(username: "richard", email: "richard@richard.com", age: 31, password: "alex123")
     user3 = User.create!(username: "ant", email: "ant@ant.com", age: 31, password: "alex123")
     user4 = User.create!(username: "chris", email: "chris@chris.com", age: 31, password: "alex123")
-    
-Board.destroy_all
-    board1 = Board.create!(name: "boardName1", description: "boardDesc1", public: true, user_id: 1)
-    board2 = Board.create!(name: "boardName2", description: "boardDesc2", public: true, user_id: 1)
 
-Pin.destroy_all
-    pin1 = Pin.create!(title: "pinTitle1", description: "pinDesc1", organized: true, user_id: 1)
-    pin2 = Pin.create!(title: "pinTitle2", description: "pinDesc2", organized: true, user_id: 1)
-    pin3 = Pin.create!(title: "pinTitle3", description: "pinDesc3", organized: true, user_id: 1)
+    board1 = Board.create!(name: "boardName1", description: "boardDesc1", public: true, user_id: user5.id)
+    board2 = Board.create!(name: "boardName2", description: "boardDesc2", public: true, user_id: user5.id)
 
-Follow.destroy_all
-    follow1 = Follow.create!(follower_id: 1, followed_id: 2)
-    follow2 = Follow.create!(follower_id: 1, followed_id: 3)
-    follow3 = Follow.create!(follower_id: 1, followed_id: 4)
-    follow4 = Follow.create!(follower_id: 1, followed_id: 5)
-    follow5 = Follow.create!(follower_id: 2, followed_id: 1)
-    follow6 = Follow.create!(follower_id: 2, followed_id: 3)
-    follow7 = Follow.create!(follower_id: 3, followed_id: 1)
-    follow8 = Follow.create!(follower_id: 3, followed_id: 2)
-    follow9 = Follow.create!(follower_id: 3, followed_id: 4)
+    pin1 = Pin.create!(title: "pinTitle1", description: "pinDesc1", organized: true, user_id: user5.id)
+    pin2 = Pin.create!(title: "pinTitle2", description: "pinDesc2", organized: true, user_id: user5.id)
+    pin3 = Pin.create!(title: "pinTitle3", description: "pinDesc3", organized: true, user_id: user5.id)
 
-Pinstoboard.destroy_all
-    pintoboard1 = Pinstoboard.create!(pin_id: 1, board_id: 1)
-    pintoboard2 = Pinstoboard.create!(pin_id: 2, board_id: 1)
-    pintoboard3 = Pinstoboard.create!(pin_id: 3, board_id: 1)
-    pintoboard4 = Pinstoboard.create!(pin_id: 1, board_id: 2)
-    pintoboard5 = Pinstoboard.create!(pin_id: 2, board_id: 2)
+    follow1 = Follow.create!(follower_id: user5.id, followed_id: user1.id)
+    follow2 = Follow.create!(follower_id: user5.id, followed_id: user2.id)
+    follow3 = Follow.create!(follower_id: user5.id, followed_id: user3.id)
+    follow4 = Follow.create!(follower_id: user5.id, followed_id: user4.id)
+    follow5 = Follow.create!(follower_id: user1.id, followed_id: user5.id)
+    follow6 = Follow.create!(follower_id: user1.id, followed_id: user2.id)
+    follow7 = Follow.create!(follower_id: user2.id, followed_id: user5.id)
+    follow8 = Follow.create!(follower_id: user2.id, followed_id: user1.id)
+    follow9 = Follow.create!(follower_id: user2.id, followed_id: user3.id)
+
+    pintoboard1 = Pinstoboard.create!(pin_id: user5.id, board_id: user5.id)
+    pintoboard2 = Pinstoboard.create!(pin_id: user1.id, board_id: user5.id)
+    pintoboard3 = Pinstoboard.create!(pin_id: user2.id, board_id: user5.id)
+    pintoboard4 = Pinstoboard.create!(pin_id: user5.id, board_id: user1.id)
+    pintoboard5 = Pinstoboard.create!(pin_id: user1.id, board_id: user1.id)
 
