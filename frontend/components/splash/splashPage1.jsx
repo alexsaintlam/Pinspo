e6658d6fd22ec627592807278fa8f7e92f4051cd
alexsaintlam <alexlam@DESKTOP-ILBQ44R.localdomain>
2021-11-03 16:08:53 -0400

    delete from splash

--- frontend/components/splash/splashPage1.jsx
+++ frontend/components/splash/splashPage1.jsx
@@ -3,10 +3,6 @@ import { connect } from 'react-redux';
 import { fetchPins } from '../../actions/pin_actions';
 
 class SplashPage1 extends React.Component {
-    // componentDidMount() {
-    //     this.props.fetchPins();
-    // }
-
     render() {
         const { pins } = this.props;
         if (Object.values(pins).length < 1) return null;
